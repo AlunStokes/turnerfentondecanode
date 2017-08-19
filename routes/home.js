9f7aaccb5208f089553c5179fa637ab801394e15
Alunsto <alunsto99@gmail.com>
2017-08-19 16:35:33 -0400

    Update home CSS to change card arrangement ruels

--- routes/home.js
+++ routes/home.js
@@ -7,7 +7,7 @@ router.use(require("./back"));
 var loadJS = require("../middlewares/loadJS");
 
 router.use(loadJS([
-  "https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"
+  
 ]));
 
 router.get("/", function(req, res, next) {
