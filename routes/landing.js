09f225b63d25cad0c91514eaee0af88a93b16cb8
MaanavD <maanav1@hotmail.com>
2017-08-25 14:29:40 -0400

    Last master branch commit with colored names

--- routes/landing.js
+++ routes/landing.js
@@ -21,50 +21,138 @@ router.get("/", function(req, res, next) {
   //the execCards array holds all the exec card objects
   res.locals.execCards = [
     {
-      name: "Exec Member",
-      position: "Executive Position",
-      decaEvent: "DECA Event",
-      decaCluster: "DECA Cluster",
-      imgPath: "images/exec-images/anon.jpg",
-      numMedals: 10,
-      numPlaques: 3
-    },
+        name: "Sonali Puri",
+        position: "President",
+        decaEvent: "BLTDM",
+        decaCluster: "Business Admin",
+        imgPath: "images/exec-images/anon.jpg",
+      },
+      {
+        name: "Bani Arora",
+        position: "Vice President",
+        decaEvent: "AAM",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Maanav Dalal",
+        position: "VP of Communications",
+        decaEvent: "BSM",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Kabisan Tanedran",
+        position: "VP of Communications",
+        decaEvent: "BSM",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Dhvani Doshi",
+        position: "VP of Education",
+        decaEvent: "BLTDM",
+        decaCluster: "Business Admin",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Kavya Kadi",
+        position: "VP of Education",
+        decaEvent: "BSM",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/Kavya.png",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Khubi Shah",
+        position: "VP of Operations",
+        decaEvent: "PFL",
+        decaCluster: "Finance",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Richard Li",
+        position: "VP of Finance",
+        decaEvent: "PFL",
+        decaCluster: "Finance",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
     {
-      name: "Exec Member",
-      position: "Executive Position",
-      decaEvent: "DECA Event",
-      decaCluster: "DECA Cluster",
+      name: "Tanzim Pathan",
+      position: "Writtens Director",
+      decaEvent: "HTPS",
+      decaCluster: "Hospitality",
       imgPath: "images/exec-images/anon.jpg",
-      numMedals: 6,
-      numPlaques: 1
+      numMedals: 0,
+      numPlaques: 0
     },
-    {
-      name: "Exec Member",
-      position: "Executive Position",
-      decaEvent: "DECA Event",
-      decaCluster: "DECA Cluster",
-      imgPath: "images/exec-images/anon.jpg",
-      numMedals: 8,
-      numPlaques: 3
-    },
-    {
-      name: "Exec Member",
-      position: "Executive Position",
-      decaEvent: "DECA Event",
-      decaCluster: "DECA Cluster",
-      imgPath: "images/exec-images/anon.jpg",
-      numMedals: 8,
-      numPlaques: 2
-    },
-    {
-      name: "Exec Member",
-      position: "Executive Position",
-      decaEvent: "DECA Event",
-      decaCluster: "DECA Cluster",
-      imgPath: "images/exec-images/anon.jpg",
-      numMedals: 12,
-      numPlaques: 2
-    },
+      {
+        name: "Hina Rani",
+        position: "Edu - Marketing",
+        decaEvent: "MCS",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/Hina.png",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Vanit Shah",
+        position: "Edu - Business Admin",
+        decaEvent: "MTDM",
+        decaCluster: "Business Admin",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Rashi R.",
+        position: "Edu - Hospitality",
+        decaEvent: "HLM",
+        decaCluster: "Hospitality",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Poonam Parmar",
+        position: "Business Associate",
+        decaEvent: "PMK",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Dalraj Dhillon",
+        position: "Business Associate",
+        decaEvent: "PMK",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/anon.jpg",
+        numMedals: 0,
+        numPlaques: 0
+      },
+      {
+        name: "Purva Vyas",
+        position: "Business Associate",
+        decaEvent: "PMK",
+        decaCluster: "Marketing",
+        imgPath: "images/exec-images/Purva.png",
+        numMedals: 0,
+        numPlaques: 0
+      }
   ]
 
   res.render("landing");
