cafed74ddfae8cf48d213c800801f82e02c263e7
Alun Stokes <alunsto99@gmail.com>
2017-09-08 17:50:50 -0400

    Add select to choose specific exam to view in chapter exam statistics

--- middlewares/loadChapterExamData.js
+++ middlewares/loadChapterExamData.js
@@ -1,14 +1,26 @@
 var Statistics = require("../models/statistics");
 
 module.exports = function(req, res, next) {
-  Statistics.getChapterExamResults(function(err, examResults) {
-    if (err) {
-      res.locals.errors.push(err);
+  var examid = req.query.examid;
+  if (typeof(examid) == "undefined") {
+    examid = -1;
+  }
+  Statistics.getExamids(function(err, examids) {
+    for (var i = 0; i < examids.length; i++) {
+      if (examids[i].id == examid) {
+        examids[i].selected = true;
+      }
+    }
+    res.locals.examids = examids;
+    Statistics.getChapterExamResults(examid, function(err, examResults) {
+      if (err) {
+        res.locals.errors.push(err);
+        next();
+        return;
+      }
+      res.locals.examResults = examResults;
       next();
       return;
-    }
-    res.locals.examResults = examResults;
-    next();
-    return;
+    });
   });
 }
