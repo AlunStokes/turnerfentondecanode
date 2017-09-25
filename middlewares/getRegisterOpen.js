c1b6d491e758bdac7245e3a35ee2bf3f265516c7
Alun Stokes <alunsto99@gmail.com>
2017-09-24 21:49:30 -0400

    Fix checking registration open middleware

--- middlewares/getRegisterOpen.js
+++ middlewares/getRegisterOpen.js
@@ -3,10 +3,10 @@ var WebsiteSettings = require("../models/websiteSettings");
 //Check if registration is currently open
 module.exports = function(req, res, next) {
   WebsiteSettings.isRegisterOpen(function(err, open) {
-    /*if (err || !open) {
+    if (err || !open) {
       res.render("registerClosed");
       return;
-    }*/
+    }
     next();
   });
 }
